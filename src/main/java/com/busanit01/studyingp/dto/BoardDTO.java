15e2395d2417204dc0b94adc568db6c03e4337b1
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2021-05-04 14:10:32 +0900

    초기 설정 및 java src 추가

---
+++ src/main/java/com/busanit01/studyingp/dto/BoardDTO.java
@@ -0,0 +1,41 @@
+package com.busanit01.studyingp.dto;
+
+import java.sql.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class BoardDTO {
+
+	// 글번호
+	private int brd_code;
+	// 글제목
+	private String brd_title;
+	// 조회수
+	private int brd_hit;
+	// 작성일
+	private Date brd_date;
+	// 글내용
+	private String brd_content;
+	// 회원번호
+	private int mem_code;
+	// 강의코드
+	private int cls_code;
+	// 말머리
+	private int brd_category;
+	// 삭제여부
+	private int brd_delete;
+	// 강의평점
+	private int brd_rating;
+
+    
+}
---
+++ src/main/java/com/busanit01/studyingp/dto/MemberDTO.java
@@ -0,0 +1,37 @@
+package com.busanit01.studyingp.dto;
+
+import java.sql.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class MemberDTO {
+
+	// 회원번호
+    private int mem_code = 0;
+    // 아이디
+    private String mem_id = null;
+    // 비밀번호
+    private String mem_pw = null;
+    // 이름
+    private String mem_name = null;
+    // 전화번호
+    private String mem_phone = null;
+    // 이메일
+    private String mem_email = null;
+    // 권한
+    private int mem_access = 0;
+    // 가입일
+    private Date mem_date = null;
+    // 탈퇴여부
+    private int mem_status = 0;    
+}
