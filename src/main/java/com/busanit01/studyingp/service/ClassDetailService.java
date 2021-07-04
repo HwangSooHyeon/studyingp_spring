7a27eda666ada129ce184c98a8be723648998e69
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2021-07-04 17:54:50 +0900

    21/07/04 db 이동 및 jdbc 수정
    db를 우분투로 이동시키고 jdbc localhost 부분을 ddns에 맞춰 수정

--- src/main/java/com/busanit01/studyingp/service/ClassDetailService.java
+++ src/main/java/com/busanit01/studyingp/service/ClassDetailService.java
@@ -1,14 +1,8 @@
 package com.busanit01.studyingp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.busanit01.studyingp.dao.ClassDetailDAO;
 
 @Service
 public class ClassDetailService {
 
-	@Autowired
-	private ClassDetailDAO clsDetailDAO;
-	
 }
